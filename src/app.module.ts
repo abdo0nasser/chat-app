ae5fa2fbf354b5183e10f4feba64831f18497f01
Abd-El-Rahman Nasser <abdonasser865@gmail.com>
2024-07-21 05:51:42 +0300

    feat(general): setup the cache manager with redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { PrismaService } from './modules/prisma/prisma.service';
@@ -8,13 +8,31 @@ import { APP_FILTER, APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './modules/auth/guards/auth.guard';
 import { HttpExceptionFilter } from './exception-filters/global.filter';
 import { PrismaExceptionFilter } from './exception-filters/prisma.filter';
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-yet';
+import type { RedisClientOptions } from 'redis';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
     UsersModule,
     PrismaModule,
+    CacheModule.registerAsync<RedisClientOptions>({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        store: await redisStore({
+          ttl: parseInt(configService.get<string>('REDIS_TTL')),
+          password: configService.get<string>('REDIS_PASSWORD'),
+          socket: {
+            host: configService.get<string>('REDIS_HOST'),
+            port: parseInt(configService.get<string>('REDIS_PORT')),
+          },
+        }),
+      }),
+      isGlobal: true,
+    }),
+    ConfigModule.forRoot({ isGlobal: true }),
   ],
   controllers: [],
   providers: [
