d6f9b23736cf84783311444cc9d52eca78aff479
Abd-El-Rahman Nasser <abdonasser865@gmail.com>
2024-08-08 07:30:54 +0300

    fix(chats): remove created at from serialized chat

--- src/modules/chats/serialized-types/serialized-chat.ts
+++ src/modules/chats/serialized-types/serialized-chat.ts
@@ -14,15 +14,8 @@ export class SerializedChat {
   })
   Description: string;
 
-  @ApiProperty({
-    example: '2021-09-20T00:00:00.000Z',
-    description: 'Chat creation date',
-  })
-  CreatedAt: Date;
-
   constructor(partial: Partial<chat>) {
     this.Title = partial.title;
-    this.Description = partial.description;
-    this.CreatedAt = partial.created_at;
+    this.Description = partial.description ? partial.description : '';
   }
 }
