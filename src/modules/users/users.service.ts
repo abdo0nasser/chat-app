6882bf7acf583873f69e324082d53ea3eea8a620
Abdo Nasser <abdonasser865@gmail.com>
2024-08-27 07:56:39 +0300

    feat(users): add find users services

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { PaginationDto } from 'src/common/pagination.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  findOne(username: string) {
+    const user = this.prismaService.user.findUnique({
+      where: {
+        username,
+      },
+    });
+    if (!user) {
+      throw new BadRequestException('User not found');
+    }
+    return user;
+  }
+
+  findAll(paginationDto: PaginationDto) {
+    const users = this.prismaService.user.findMany({
+      skip: paginationDto.page * paginationDto.limit,
+      take: paginationDto.limit,
+    });
+    if (!users) throw new BadRequestException('No users found');
+    return users;
+  }
+}
